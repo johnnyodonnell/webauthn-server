0bfffcf792924cb50c845a7a593663c6e4d1ad50
Johnny O'Donnell <odjohnny@amazon.com>
2019-11-14 10:24:50 -0800

    Post credential to server
    Credential still needs to be verified

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const uuid = require("uuid/v4");
 const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
 
 
 const userId = "odjohnny";
+
+const registrationRequests = {};
+const registeredKeys = {};
 
 const app = express();
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
     const challenge = uuid();
+
+    registrationRequests[userId] = challenge;
 
     res.render("index.html.mustache", { userId, challenge, });
 });
 
 app.post("/register", (req, res) => {
+    const credential = req.body;
+    console.log(credential);
+
+    res.json(true);
 });
 
 app.listen(3000, () => {
