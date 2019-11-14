67c9acc5cb0a74bcef4af9455806e6554cc67c40
Johnny O'Donnell <odjohnny@amazon.com>
2019-11-13 19:26:15 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Success!");
+});
+
+app.listen(3000, () => {
+    console.log("Started server...");
+});
+
