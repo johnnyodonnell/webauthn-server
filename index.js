57b532a67cb85c9b9dc905aad720510c3938e12c
Johnny O'Donnell <odjohnny@amazon.com>
2019-11-14 09:32:12 -0800

    Add very basic registration flow with server state or verification

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const uuid = require("uuid/v4");
+const mustacheExpress = require("mustache-express");
 
+
+const userId = "odjohnny";
 
 const app = express();
 
-app.use(express.static("public"));
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
 
-app.get("/ping", (req, res) => {
-    res.send("Success!");
+app.get("/", (req, res) => {
+    const challenge = uuid();
+
+    res.render("index.html.mustache", { userId, challenge, });
+});
+
+app.post("/register", (req, res) => {
 });
 
 app.listen(3000, () => {
